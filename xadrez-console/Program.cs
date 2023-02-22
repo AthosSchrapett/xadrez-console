7abf7fc936bff6130b00bff2bddbc65227d3224d
Athos Schrapett <athosschrapett@outlook.com>
2023-02-22 10:39:56 -0300

    feat: criando a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,19 +4,24 @@ using xadrez_console.Tabuleiro.Enum;
 using xadrez_console.Tabuleiro.Exceptions;
 using xadrez_console.Xadrez;
 
-try
-{
-    TabuleiroTab tab = new(8, 8);
+//try
+//{
+//    TabuleiroTab tab = new(8, 8);
 
-    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-    tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+//    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+//    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+//    tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
 
-    Tela.ImprimirTabuleiro(tab);
-}
-catch (TabuleiroException e)
-{
-    Console.WriteLine(e.Message);
-}
+//    Tela.ImprimirTabuleiro(tab);
+//}
+//catch (TabuleiroException e)
+//{
+//    Console.WriteLine(e.Message);
+//}
+
+PosicaoXadrez posicaoXadrez = new('c', 7);
+
+Console.WriteLine(posicaoXadrez);
+Console.WriteLine(posicaoXadrez.ToPosicao());
 
 Console.ReadLine();
